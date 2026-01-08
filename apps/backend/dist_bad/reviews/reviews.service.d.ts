a06efb3a5d3acaacca743a82e56f78644c51dd14
donthigarivinay <vinaykumaryadav.2112@gmail.com>
2026-01-08 20:22:48 +0530

    db

--- apps/backend/dist_bad/reviews/reviews.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-export declare class ReviewsService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    createReview(data: any): Promise<{
-        message: string;
-    }>;
-    getStudentReviews(studentId: string): Promise<never[]>;
-}
